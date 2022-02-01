7485175a33ee8d35627cc1f16968fbea22f97e67
Luijika711 <88895321+Luijika711@users.noreply.github.com>
2022-02-01 12:21:55 +0200

    Add files via upload

---
+++ typing_test(DACA IL STERGI NUSH CE ITI FACC)/main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<windows.h>
+#include<stdlib.h>
+#include<time.h>
+#include<fstream>
+#include<chrono>
+//include files :D
+
+std::string addrandomwords()
+{
+    std::string coolstring;
+    //open text file,put them into array,choose random amount of words,put them into coolstring
+    return coolstring;
+}
+void typing()
+{
+    //do typing test itself
+}
+
+int main()
+{
+
+    return 0;
+}
